c13ca1030f4e43b5ff26ae0b3c6ac356c03d06c1
Ekaterina <vipolsovatel@mail.ru>
2022-05-15 14:46:11 +0300

    Reformat code

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        CreditPaymentService cps = new CreditPaymentService();
+        CreditPaymentService service = new CreditPaymentService();
 
-        double p = cps.monthlyPayment(1, 1_000_000, 9.99); //при years 2, p = 46140, при years 3, p = 32262
-        System.out.println(Math.round(p));
+        double payMonth = service.monthlyPayment(1, 1_000_000, 9.99); //при years 2, p = 46140, при years 3, p = 32262
+        System.out.println(Math.round(payMonth));
     }
 
 }
